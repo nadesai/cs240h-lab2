6045c93a4d0dcbf934d191a64bf2c5b1b1e575e3
Nikhil Desai <nikhild@stanford.edu>
2014-04-19 15:51:06 -0700

    adding network and bytestring dependencies to cabal file

--- Chat.hs
+++ Chat.hs
@@ -3,8 +3,11 @@ module Chat (chat) where
 
 import System.Environment
 
+portName = "CHAT_SERVER_PORT"
+
 -- | Chat server entry point.
 chat :: IO ()
 chat = do
-     chatPort <- getEnv "CHAT_SERVER_PORT"
+     chatPort <- getEnv portName
      putStrLn chatPort
+
