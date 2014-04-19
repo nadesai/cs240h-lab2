1be3224fb6e204425106868fe48d03a4eabe66c4
Nikhil Desai <nikhild@stanford.edu>
2014-04-19 12:53:44 -0700

    Logic for obtaining CHAT_SERVER_PORT

--- Chat.hs
+++ Chat.hs
@@ -1,7 +1,10 @@
 -- | CS240h Lab 2 Chat Server
 module Chat (chat) where
 
+import System.Environment
+
 -- | Chat server entry point.
 chat :: IO ()
-chat = return ()
-
+chat = do
+     chatPort <- getEnv "CHAT_SERVER_PORT"
+     putStrLn chatPort
