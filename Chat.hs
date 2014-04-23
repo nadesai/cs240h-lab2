1fb5c1be6a3226ef56c9a33e9537e6217b3a62fe
Nikhil Desai <nikhild@stanford.edu>
2014-04-22 21:39:22 -0700

    some more stuff - almost working

--- Chat.hs
+++ Chat.hs
@@ -1,69 +1,73 @@
 -- | CS240h Lab 2 Chat Server
 module Chat (chat) where
 
-import Network (listenOn, accept, PortID(..), PortNumber, Socket)
-import System.IO (hSetBuffering, hGetLine, hPutStrLn, BufferMode(..), Handle)
-import Control.Concurrent (forkIO)
-import Control.Concurrent.Chan
+import Network (PortID(..), PortNumber, Socket, listenOn, accept)
+import System.IO (BufferMode(..), Handle, utf8, hSetBuffering, hGetLine, hPutStrLn, hSetEncoding, hClose)
 import System.Environment (getEnv)
+import Control.Monad (forever)
+import Control.Concurrent (forkIO, forkFinally, killThread)
+import Control.Concurrent.Chan (Chan, newChan, dupChan, writeChan, readChan) 
+import Control.Exception (IOException, catch)
 
+-- Type alias: a user's identity is fully specified by their integer ID.
 type UserID = Integer
 
+-- A message can be a join notification, a message from a user to the chatroom, or a departure
+-- notification. 
 data Message = Join UserID | Msg UserID String | Leave UserID
 instance Show Message where
     show (Join i) = show i ++ " has joined"
     show (Msg i m) = show i ++ ": " ++ m
     show (Leave i) = show i ++ " has left"
 
--- import Chat
+-- The name of the environment variable which stores the port we want to read.
 portName :: String
 portName = "CHAT_SERVER_PORT"
 
+-- The main function of the module; starts a chat server on the given port.
 chat :: IO ()
 chat = do
-    putStrLn $ "Server started."
     chatPort <- getEnv portName
     let portNum = (fromIntegral :: Integer -> PortNumber) $ read chatPort
     sock <- listenOn $ PortNumber portNum
 
     chan <- newChan
-    _ <- forkIO $ printLog chan
-    handleNewConnections chan sock 0
+    _ <- forkIO $ clearLog chan
+    handleNewConnections chan sock 1
 
-printLog :: Chan Message -> IO ()
-printLog chan = do
-    msg <- readChan chan
-    putStrLn $ show msg
-    printLog chan
+-- Each channel must be read in its entirety, so clearLog clears the master channel.
+clearLog :: Chan Message -> IO ()
+clearLog chan = forever $ readChan chan
 
 handleNewConnections :: Chan Message -> Socket -> UserID -> IO ()
 handleNewConnections chan sock i = do
     (client, _, _) <- accept sock
     hSetBuffering client NoBuffering
+    hSetEncoding client utf8
 
     chan2 <- dupChan chan
-    _ <- forkIO $ serveChats client chan2 i
-    handleNewConnections chan sock $! (i+1)
+    _ <- forkFinally (serveChats client chan2 i) (\_ -> hClose client)
+    let nextID = i+1
+    handleNewConnections chan sock $! nextID
 
 serveChats :: Handle -> Chan Message -> UserID -> IO ()
-serveChats client chan i =
-    do
-        writeChan chan (Join i)
+serveChats client chan i = do
         chan2 <- dupChan chan
-        _ <- forkIO $ handleClientInput client chan2 i
-        printClientLoop client chan i
+        writeChan chan (Join i)
+        tid <- forkIO $ printClientLoop client chan2 i
+        handleClientInput client chan i `catch` \e -> do 
+            let _ = (e :: IOException)
+            killThread tid
+            writeChan chan (Leave i)
 
 handleClientInput :: Handle -> Chan Message -> UserID -> IO ()
-handleClientInput client chan i = do
+handleClientInput client chan i = forever $ do
     line <- hGetLine client
-    let msg = Msg i line
-    writeChan chan msg
-    handleClientInput client chan i
+    writeChan chan (Msg i line)
 
 printClientLoop :: Handle -> Chan Message -> UserID -> IO ()
-printClientLoop client chan i = do
+printClientLoop client chan i = forever $ do
     msg <- readChan chan
     case msg of
         (Msg j _) | j == i -> return ()
         _ -> hPutStrLn client $ show msg
-    printClientLoop client chan i
